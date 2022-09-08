a6a600ba55e1dbbd0b8135902e9a4d8bd8727405
Darya Osipova <domali555@yandex.ru>
2022-09-08 22:01:06 +0300

    первыйкоммит

---
+++ script/index.js
@@ -0,0 +1,39 @@
+let formElement = document.querySelector('.popup__container');
+let nameInput = document.querySelector('.popup__input_name_name');
+let jobInput = document.querySelector('.popup__input_name_job');
+
+let titleName = document.querySelector('.profile__name');
+let titleJob = document.querySelector('.profile__job');
+
+const openPopUp = document.querySelector('.profile__edit');
+const closePopUp = document.querySelector('.popup__close-icon');
+const popup = document.querySelector('.popup');
+
+
+openPopUp.addEventListener('click', () => {
+    popup.classList.add('active');
+});
+
+closePopUp.addEventListener('click', () => {
+    popup.classList.remove('active');
+});
+
+console.log(openPopUp, closePopUp);
+
+formElement.addEventListener('submit', formSubmitHandler);
+
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+
+    const nameValue = nameInput.value;
+    const jobValue = jobInput.value;
+
+    titleName.textContent = nameValue;
+    titleJob.textContent = jobValue;
+    popup.classList.add('popup_closed');
+}
+
+
+
+console.log(formElement, nameInput, jobInput);
