254c4b16d25c8c8c02436a73e6b52652746ea71f
Darya Osipova <domali555@yandex.ru>
2022-09-10 23:46:26 +0300

    новыйкоммит1

--- script/index.js
+++ script/index.js
@@ -2,39 +2,34 @@ let formElement = document.querySelector('.popup__container');
 let nameInput = document.querySelector('.popup__input_name_name');
 let jobInput = document.querySelector('.popup__input_name_job');
 
+
 let titleName = document.querySelector('.profile__name');
 let titleJob = document.querySelector('.profile__job');
 
-const openPopUp = document.querySelector('.profile__edit');
-const closePopUp = document.querySelector('.popup__close-icon');
+const buttonOpenPopUp = document.querySelector('.profile__edit');
+const buttonClosePopUp = document.querySelector('.popup__close-icon');
 const popup = document.querySelector('.popup');
 
+buttonOpenPopUp.addEventListener('click', openPopUp);
+buttonClosePopUp.addEventListener('click', closePopUp);
 
-openPopUp.addEventListener('click', () => {
+function openPopUp() {
     popup.classList.add('popup_opened');
+    nameInput.value = titleName.textContent;
+    jobInput.value = titleJob.textContent;
 
-});
+};
 
-closePopUp.addEventListener('click', () => {
+function closePopUp() {
     popup.classList.remove('popup_opened');
-});
-
-console.log(openPopUp, closePopUp);
+};
 
 formElement.addEventListener('submit', formSubmitHandler);
 
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
-
-    const nameValue = nameInput.value;
-    const jobValue = jobInput.value;
-
-    titleName.textContent = nameValue;
-    titleJob.textContent = jobValue;
-    popup.classList.remove('popup_opened');
+    titleJob.textContent = nameInput.value;
+    titleName.textContent = jobInput.value;
+    closePopUp();
 }
-
-
-
-console.log(formElement, nameInput, jobInput);
